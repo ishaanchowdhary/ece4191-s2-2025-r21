fa202745e2cc73de15be0ece0f76782590183962
ishaanchowdhary <ishaanguhachowdhary@gmail.com>
2025-08-26 09:07:10 +1000

    Create model_test.py
    Tests models from video or live camera feed

---
+++ test_scripts/model_test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+VIDEO_PATH = "object_detection/data_collection/videos/WIN_20250808_10_49_48_Pro.mp4"  # Change to your test video file
+MODEL_PATH = "models/best_v1.pt"  # Change to your YOLO model file
+USE_CAM = False
+
+
+def annotate_image(image, detections):
+    """Annotates the image with bounding boxes."""
+    for det in detections:
+        x1, y1, x2, y2 = det["x1"], det["y1"], det["x2"], det["y2"]
+        label = f"{det['label']}: {det['confidence']:.2f}"
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)        
+    return image
+
+def run_yolo(model, images):
+    """Runs YOLO model on a batch of images."""
+    results = model(images)
+    detections = []
+
+    for result in results:
+        detection_list = []
+        for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            confidence = box.conf.item()
+            class_id = int(box.cls)
+            detection_list.append({
+                "label": model.names[class_id],
+                "confidence": confidence,
+                "x1": x1, "y1": y1, "x2": x2, "y2": y2
+            })
+
+        detections.append(detection_list)
+
+    return detections
+
+if __name__ == '__main__':
+    print("Get on the beers bruh\n|\|\ \n(-.-)\n|> >\n\|\|")
+    model = YOLO(MODEL_PATH)
+    if USE_CAM == True:
+        cap = cv2.VideoCapture(0)
+    else:
+        cap = cv2.VideoCapture(VIDEO_PATH)
+    
+    while cap.isOpened():
+        try:
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            detections = run_yolo(model, frame)            
+            annotated_frame = annotate_image(frame, detections[0])
+            cv2.imshow("YOLO Animal Detection", annotated_frame)
+
+            cv2.waitKey(1)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except KeyboardInterrupt:
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
