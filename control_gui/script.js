4bfb98f7c1ae70bb791611cee24bec242ddcc10d
mgil0015 <mgil0015@student.monash.edu>
2025-09-01 15:35:33 +1000

    Added michaels iphone to network list

--- control_gui/script.js
+++ control_gui/script.js
@@ -21,7 +21,8 @@
 //const RPI_IP = "192.168.4.110"; // Pi's LAN IP (or hostname): Ishaan's house wifi (i think)
 // const RPI_IP = "192.168.20.12"; // Pi's LAN IP (or hostname): Jaiden and Liv's house wifi (i think)
 //const RPI_IP = "172.20.10.3"; // Pi's LAN IP (or hostname): Jaiden's iPhone
-const RPI_IP = "192.168.20.50"; // Pi's LAN IP (or hostname): Michael's house wifi
+//const RPI_IP = "192.168.20.50"; // Pi's LAN IP (or hostname): Michael's house wifi
+const RPI_IP = "172.20.10.4"; // Pi's LAN IP (or hostname): Michael's iPhone
 
 // Websocket Ports
 const CMD_PORT = 9000;        // WebSocket Port for commands
