032ca2b76c840badc12d9415a6f72d2b5b45e0a8
ishaanchowdhary <ishaanguhachowdhary@gmail.com>
2025-10-10 12:42:00 +1100

    ya
    ya

--- script/config.py
+++ script/config.py
@@ -38,7 +38,7 @@ RIGHT_PWM, RIGHT_IN1, RIGHT_IN2 = 13, 8, 7
 
 # Camera defaults (used by camera_stream)
 CAM_INDEX = 0
-CAM_WIDTH = 1280
-CAM_HEIGHT = 720
+CAM_WIDTH = 640
+CAM_HEIGHT = 640
 CAM_FPS = 25
 JPEG_QUALITY = 50
